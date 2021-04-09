f0700ed37610b275285e82160f3d7bc136d6faea
Kwades <bz.jasiek@gmail.com>
2021-04-09 22:02:23 +0200

    feat: 🎸 Initilize the project

---
+++ PopTheCob/PopTheCob/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CobGotTouched(_ sender: UIButton) {
+        print("Cob got touched")
+    }
+
+
+}
+
